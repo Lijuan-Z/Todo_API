4c127149b0d265c47e41fccb3f41f557038b6c0e
Lijuan Zhu <lisazhuv@gmail.com>
2023-03-26 20:40:54 -0700

    change model file name to lower case

---
+++ models/todo.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema(
+  {
+    description: { 
+      type: String, 
+      required: true
+     },
+    category: { 
+      type: String, 
+      required: true 
+    },
+    completed: { 
+      type: Boolean, 
+      default: false 
+    },
+    owner: { 
+      type: String, 
+      ref: "User", 
+      required: true 
+    },
+  },
+  { 
+    timestamps: true 
+  }
+);
+
+module.exports = mongoose.model("Todo", todoSchema);
